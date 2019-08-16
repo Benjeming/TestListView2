7198368848bc47e54da36d2ca97aa86317310eee
ludans <1243515833@qq.com>
2019-08-16 21:11:18 +0800

    Test

--- app/src/main/java/FragmentPager/NewsContentFragment.java
+++ app/src/main/java/FragmentPager/NewsContentFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,7 +30,7 @@ public class NewsContentFragment extends Fragment {
 
         Bundle bundle = getArguments();
 //        String title = bundle.getString("title");
-//        Log.e(TAG, "Fragment 中的值" + data);
+        Log.e(TAG, "Fragment 中的值");
 
         NewsTitle temp = (NewsTitle) bundle.getSerializable("title");
         textView.setText(temp.getTitle());
